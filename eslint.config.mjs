60b66d2b1b5061a14b542e9450ec12f2b1dfcd0a
Aiden <bokjaeoh@gmail.com>
2025-01-02 20:01:35 +0900

    Update `eslint.config.mjs` for the `prettier`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,24 @@
 import pluginJs from "@eslint/js"
+import prettierConfig from "eslint-config-prettier"
+import prettierPlugin from "eslint-plugin-prettier"
 import globals from "globals"
 
 export default [
   pluginJs.configs.recommended,
+  prettierConfig,
   {
-    extends: ["prettier"],
     files: ["**/*.{cjs,js,jsx,mjs,ts,tsx}"],
     languageOptions: {
       globals: {
         ...globals.es2020,
         ...globals.jest,
         ...globals.node,
+        log: "writable",
       },
+      parserOptions: { ecmaVersion: "latest" },
     },
-    parserOptions: { ecmaVersion: "latest" },
-    plugins: ["prettier"],
-    rules: {},
+    plugins: { prettier: prettierPlugin },
+    rules: { "prettier/prettier": "error" },
     settings: {},
   },
   {
